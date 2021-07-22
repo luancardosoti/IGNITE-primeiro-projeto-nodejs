e4ec81981ec91a91d6fb4064567fd62322cbf880
luancardosoti <luancardosoti@gmail.com>
2021-07-22 00:53:39 -0300

    adicionando requisito de criaçao de conta obedecendo as suas regras de negocios

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+app.use(express.json());
+
+const customers = [];
+/**
+ * cpf - string
+ * name - string
+ * id - uuid
+ * statement []
+ */
+app.post('/accounts', (request, response) => {
+  const { name, cpf } = request.body;
+
+  const customerAlreadyExists = customers.some(
+    (customer) => customer.cpf === cpf
+  );
+
+  if (customerAlreadyExists) {
+    return response.status(400).json({ error: 'Customer Already Exists.' });
+  }
+
+  customers.push({
+    name,
+    cpf,
+    id: uuidv4(),
+    statement: [],
+  });
+
+  return response.status(201).send();
+});
 
 app.listen(3333);
